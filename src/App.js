3877fa06d2e7986c275463a9961da5eaa55febd3
Rahul singh <100202443+Rahulcse79@users.noreply.github.com>
2023-09-01 18:42:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -276,10 +276,7 @@ class App extends React.Component {
                   <div className="col-5 text-right text-light border-right py-3">
                     <div className="m-auto"><i className="ti-heart-broken icon-xl" /></div>
                   </div>
-                  <div className="col-7 text-left py-3">
-                    <h1 className="text-danger font-weight-bold font40">2k</h1>
-                    <p className="text-light mb-1">Coffee Drinked</p>
-                  </div>
+                  
                 </div>
               </div>
             </div>
